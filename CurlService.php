30cadf7128f378c82649b3962b0acfe62c1e6fde
Marcelo Alves <marceloalvessoft@gmail.com>
2020-07-03 16:45:23 -0300

    result

--- CurlService.php
+++ CurlService.php
@@ -40,7 +40,6 @@ class CurlService
         curl_setopt($ch, CURLOPT_URL, $url);
         $object = curl_exec($ch);
 
-        dd($object);
 
         if ($object === false) {
             throw new \Exception('Curl error: ' . curl_error($ch));
