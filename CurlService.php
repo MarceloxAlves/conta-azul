3adfba611f1216b3e19a594643da6c68dc9cc3ec
Marcelo Alves <marceloalvessoft@gmail.com>
2020-07-03 17:11:11 -0300

    result

--- CurlService.php
+++ CurlService.php
@@ -40,6 +40,7 @@ class CurlService
         curl_setopt($ch, CURLOPT_URL, $url);
         $object = curl_exec($ch);
 
+
         if ($object === false) {
             throw new \Exception('Curl error: ' . curl_error($ch));
         }
--- index.php
+++ index.php
@@ -8,89 +8,93 @@ function dd($param)
 }
 
 require_once 'ContaAzulService.php';
+try {
+    $contaAzul = new ContaAzulService();
 
-$contaAzul = new ContaAzulService();
-
-if ($_GET['code']) {
-    $_SESSION['token_conta_azul'] = $_GET['code'];
-}
-
-if (isset($_SESSION['token_conta_azul'])) {
-    echo "Logado";
-    if (!$_SESSION['access_token']) {
-        $token = $contaAzul->getToken($_SESSION['token_conta_azul']);
-        $contaAzul->saveSessions($token);
-
+    if ($_GET['code']) {
+        $_SESSION['token_conta_azul'] = $_GET['code'];
     }
-    if ($_SESSION['access_token']) {
-        $token = $contaAzul->refreshToken();
-        $contaAzul->saveSessions($token);
-        $applications = Conexao::readSQL("select * from aplicacao app 
+
+    if (isset($_SESSION['token_conta_azul'])) {
+        echo "Logado";
+        if (!$_SESSION['access_token']) {
+            $token = $contaAzul->getToken($_SESSION['token_conta_azul']);
+            $contaAzul->saveSessions($token);
+
+        }
+        if ($_SESSION['access_token']) {
+            $token = $contaAzul->refreshToken();
+            $contaAzul->saveSessions($token);
+            $applications = Conexao::readSQL("select * from aplicacao app 
 where paciente != '0' and idExclusao is not null");
-        $pacienteArray = array();
-        foreach ($applications as $application) {
-            $paciente = $application['paciente'];
-            $idExclusao = $application['idExclusao'];
-            $pacienteArray[$paciente][$idExclusao][] = $application;
-        }
-
-        foreach ($pacienteArray as $key => $paciente) {
-            $sale = array(
-                'number' => $application['idExclusao'],
-                'emission' => date('Y-m-dTH:i:sZ'),
-                'status' => 'COMMITTED',
-                'customer_id' => $key,
-                'products' => array(),
-                'discount' => array(),
-                'payment' =>
-                    array(
-                        'type' => 'CASH',
-                        'installments' => array(),
-                    ),
-                'notes' => '',
-                'shipping_cost' => 0,
-            );
-            $total = 0;
-            foreach ($paciente as $keyIdExclusao => $idExclusao) {
-
-                foreach ($idExclusao as $product) {
-                    $sale["products"][] = [
-                        "quantity" => $product["dose"],
-                        "product_id" => $product["vacina"],
-                        "value" => $product["valorDose"],
-                    ];
-                }
-
-                $receitas = Conexao::readSQL("select * from receitas rc where idExclusao = '$keyIdExclusao'");
-                if (count($receitas) > 1) {
-                    $sale["payment"]["type"] = "TIMES";
-                }
-
-                $desconto = 0;
-
-                foreach ($receitas as $parcela => $receita) {
-                    $desconto += $receita["desconto"];
-                    $total += $receita["valor"];
-                    $sale["payment"]["installments"][] = [
-                        "number" => ++$parcela,
-                        "due_date" => $receita['data_venc'],
-                        "value" => $receita['valor'],
-                    ];
-                }
-
-                $sale["discount"] = [
-                    "measure_unit" => "VALUE",
-                    "rate" => $desconto,
-                ];
-
-                $sale["shipping_cost"] = $total;
+            $pacienteArray = array();
+            foreach ($applications as $application) {
+                $paciente = $application['paciente'];
+                $idExclusao = $application['idExclusao'];
+                $pacienteArray[$paciente][$idExclusao][] = $application;
             }
-           $result =  $contaAzul->createSale($sale);
-            var_dump($result);
+
+            foreach ($pacienteArray as $key => $paciente) {
+                $sale = array(
+                    'number' => $application['idExclusao'],
+                    'emission' => date('Y-m-dTH:i:sZ'),
+                    'status' => 'COMMITTED',
+                    'customer_id' => $key,
+                    'products' => array(),
+                    'discount' => array(),
+                    'payment' =>
+                        array(
+                            'type' => 'CASH',
+                            'installments' => array(),
+                        ),
+                    'notes' => '',
+                    'shipping_cost' => 0,
+                );
+                $total = 0;
+                foreach ($paciente as $keyIdExclusao => $idExclusao) {
+
+                    foreach ($idExclusao as $product) {
+                        $sale["products"][] = [
+                            "quantity" => $product["dose"],
+                            "product_id" => $product["vacina"],
+                            "value" => $product["valorDose"],
+                        ];
+                    }
+
+                    $receitas = Conexao::readSQL("select * from receitas rc where idExclusao = '$keyIdExclusao'");
+                    if (count($receitas) > 1) {
+                        $sale["payment"]["type"] = "TIMES";
+                    }
+
+                    $desconto = 0;
+
+                    foreach ($receitas as $parcela => $receita) {
+                        $desconto += $receita["desconto"];
+                        $total += $receita["valor"];
+                        $sale["payment"]["installments"][] = [
+                            "number" => ++$parcela,
+                            "due_date" => $receita['data_venc'],
+                            "value" => $receita['valor'],
+                        ];
+                    }
+
+                    $sale["discount"] = [
+                        "measure_unit" => "VALUE",
+                        "rate" => $desconto,
+                    ];
+
+                    $sale["shipping_cost"] = $total;
+                }
+                $result = $contaAzul->createSale($sale);
+                var_dump($result);
+            }
         }
+    } else {
+        echo "<a href='./login.php'>Solicitar Permissão Conta Azul</a>";
     }
-} else {
-    echo "<a href='./login.php'>Solicitar Permissão Conta Azul</a>";
+}catch (Exception $exception){
+    echo $exception->getMessage();
+    echo  $exception->getCode();
 }
 
 
