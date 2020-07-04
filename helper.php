194b17d9cbb788996cc931d94e873469db361103
Marcelo Alves <marceloalvessoft@gmail.com>
2020-07-04 13:50:35 -0300

    result

--- helper.php
+++ helper.php
@@ -8,5 +8,5 @@ function dd($param)
 
 function dateContaAzul($date, $format='Y-m-d'){
     $dateTime = \DateTime::createFromFormat($format, $date);
-    return $dateTime->format('Y-m-d\TH:i:s.z');
+    return $dateTime->format('Y-m-d\TH:i:s.P');
 }
