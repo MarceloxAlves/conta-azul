d3b1bad8baf78952b330e778daf9f117593704be
Marcelo Alves <marceloalvessoft@gmail.com>
2020-07-04 12:47:21 -0300

    result

--- helper.php
+++ helper.php
@@ -5,8 +5,7 @@ function dd($param)
     die();
 }
 
-function dateContaAzul($date){
-    $dateTime = new DateTime($date." 00:00:00");
-    $dateTime->setTimezone(new DateTimeZone('America/New_York'));
-    return $dateTime->format('Y-m-d\TH:i:s.u');
+function dateContaAzul($date, $format='Y-m-d'){
+    $dateTime = \DateTime::createFromFormat($format, $date);
+    return $dateTime->format('Y-m-d\TH:i:s.z');
 }
